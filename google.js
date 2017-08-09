e144f02c80b201df8990222f1b2cfe8156ef7636
Kevin Parnell <kpbballer@gmail.com>
2017-08-09 15:06:58 -0400

    Delete google.js

--- google.js
+++
@@ -1,8 +0,0 @@
-window.onkeyup = keyup;
-var inputTextValue;
-function keyup(e) {
-    inputTextValue = e.target.value;
-    if (e.keyCode == 13) {
-        window.location = "http://www.google.com/search?q=" + inputTextValue;
-    }
-}
